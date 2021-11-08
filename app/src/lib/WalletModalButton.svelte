71ada0fc35b9adf352bae3784dfd6abb42d4cc54
Colin Ogoo <ogoo.colin@gmail.com>
2021-11-08 20:28:04 +0000

    feat: wallet modal button

---
+++ app/src/lib/WalletModalButton.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { walletStore } from '$utils/walletStore';
+  import WalletButton from "./WalletButton.svelte";
+
+  const selectWallet = (walletName) => {
+		$walletStore.select(walletName);
+	};
+
+</script>
+
+<WalletButton on:click={() => selectWallet('Phantom')}>
+  Select Wallet
+</WalletButton>
