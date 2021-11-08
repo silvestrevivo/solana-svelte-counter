d769e396a09b7de22775557c5176415d252dd6b0
Colin Ogoo <ogoo.colin@gmail.com>
2021-11-08 20:27:02 +0000

    feat: wallet button component

---
+++ app/src/lib/WalletButton.svelte
@@ -0,0 +1,19 @@
+<button class="wallet-adapter-button" class:justify-between={$$slots['start-icon'] || $$slots['end-icon']} on:click>
+  {#if $$slots['start-icon']}
+     <i class="wallet-adapter-button-start-icon">
+         <slot name="start-icon"></slot>
+     </i>
+  {/if}
+  <slot></slot>
+  {#if $$slots['end-icon']}
+     <i  class="wallet-adapter-button-end-icon">
+         <slot name="end-icon"></slot>
+     </i>
+  {/if}
+</button>
+
+<style>
+  .justify-between {
+    justify-content: space-between;
+  }
+</style>
