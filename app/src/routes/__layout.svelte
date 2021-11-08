a9d3f9b5ad237e07189d9ea3f8cd9e55e54189d0
Colin Ogoo <ogoo.colin@gmail.com>
2021-11-08 17:00:58 +0000

    chore(styles): add styles from react-ui starter

--- app/src/routes/__layout.svelte
+++ app/src/routes/__layout.svelte
@@ -1,9 +1,10 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
-	import { clusterApiUrl } from '@solana/web3.js';
-	import WalletProvider from '$lib/WalletProvider.svelte';
 	import ConnectionProvider from '$lib/ConnectionProvider.svelte';
+	import WalletProvider from '$lib/WalletProvider.svelte';
+	import { clusterApiUrl } from '@solana/web3.js';
+	import { onMount } from 'svelte';
 	import idl from '../../../target/idl/solana_svelte_counter.json';
+	import '../styles.css';
 
 	const localStorageKey = 'walletAdapter';
 	// const network = 'http://127.0.0.1:8899';
