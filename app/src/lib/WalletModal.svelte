efef6576b8d2300355a3e0bc16fc9652b5b275cb
Colin Ogoo <ogoo.colin@gmail.com>
2021-11-09 14:45:13 +0000

    feat: styling for modal

--- app/src/lib/WalletModal.svelte
+++ app/src/lib/WalletModal.svelte
@@ -17,15 +17,31 @@
 	class="wallet-adapter-modal wallet-adapter-modal-fade-in"
 	role="dialog"
 >
-	<h1>Connect Wallet</h1>
-	{#each $walletConfigStore.wallets as { name, icon }}
-		<WalletButton on:click={() => connect(name)}>
-			{name}
+	<div class="wallet-adapter-modal-container">
+		<div class="wallet-adapter-modal-wrapper">
+			<h1 class="wallet-adapter-modal-title">Connect Wallet</h1>
 
-			<svelte:fragment slot="end-icon">
-				<WalletIcon wallet={{ name, icon }} />
-			</svelte:fragment>
-		</WalletButton>
-	{/each}
-	<p on:click={() => dispatch('close')}>close</p>
+			<button on:click={() => dispatch('close')} class="wallet-adapter-modal-button-close">
+				<svg width="14" height="14">
+					<path
+						d="M14 12.461 8.3 6.772l5.234-5.233L12.006 0 6.772 5.234 1.54 0 0 1.539l5.234 5.233L0 12.006l1.539 1.528L6.772 8.3l5.69 5.7L14 12.461z"
+					/>
+				</svg>
+			</button>
+
+			<ul class="wallet-adapter-modal-list">
+				{#each $walletConfigStore.wallets as { name, icon }}
+					<li class="r">
+						<WalletButton on:click={() => connect(name)}>
+							{name}
+
+							<svelte:fragment slot="end-icon">
+								<WalletIcon wallet={{ name, icon }} />
+							</svelte:fragment>
+						</WalletButton>
+					</li>
+				{/each}
+			</ul>
+		</div>
+	</div>
 </div>
