7cc4851f455226e89cc2c526e7219f237f6e40e3
silvestrevivo <silvestrevivomillan@gmail.com>
2021-11-09 14:41:38 +0100

    cleaning up code

--- app/src/lib/WalletModalButton.svelte
+++ app/src/lib/WalletModalButton.svelte
@@ -1,11 +1,5 @@
 <script lang="ts">
-	// import { walletStore } from '$utils/walletStore';
 	import WalletButton from './WalletButton.svelte';
-	// const selectWallet = (walletName) => {
-	// 	$walletStore.select(walletName);
-	// };
 </script>
 
 <WalletButton class="wallet-adapter-button-trigger" on:click>Select Wallet</WalletButton>
-
-<!-- <WalletButton on:click={() => selectWallet('Phantom')}>Select Wallet</WalletButton> -->
