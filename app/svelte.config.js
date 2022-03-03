d10f1913b412c89528a64a3bf9754347c540bfb5
Colin Ogoo <ogoo.colin@gmail.com>
2022-03-03 08:14:24 +0000

    fix(anchor): console errors on dev mode (#19)

--- app/svelte.config.js
+++ app/svelte.config.js
@@ -4,24 +4,26 @@ import path from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: preprocess(),
+  preprocess: preprocess(),
 
-	kit: {
-		adapter: adapter(),
-		target: '#svelte',
-		vite: {
-			adapter: adapter(),
+  kit: {
+    adapter: adapter(),
+    vite: {
+      adapter: adapter(),
+      optimizeDeps: {
+        include: ['@project-serum/anchor'],
+      },
       resolve: {
         alias: {
           $utils: path.resolve('src/utils/'),
         },
       },
-			define: {
+      define: {
         // This makes @project-serum/anchor 's process error not happen since it replaces all instances of process.env.BROWSER with true
-        'process.env.BROWSER': true
-      }
+        'process.env.BROWSER': true,
+      },
     },
-	}
+  },
 };
 
 export default config;
