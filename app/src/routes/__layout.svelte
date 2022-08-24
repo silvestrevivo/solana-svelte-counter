ff800726b6b4874e5d23395b975a8b0bf9b3a4af
silvestrevivo <silvestrevivomillan@gmail.com>
2022-08-24 18:06:39 +0200

    feat(adapter): testing torus

--- app/src/routes/__layout.svelte
+++ app/src/routes/__layout.svelte
@@ -4,6 +4,8 @@
   import { WalletProvider } from '@svelte-on-solana/wallet-adapter-ui';
   import { AnchorConnectionProvider } from '@svelte-on-solana/wallet-adapter-anchor';
   import idl from '../../../target/idl/solana_svelte_counter.json';
+  //...
+  import { walletStore } from '@svelte-on-solana/wallet-adapter-core';
 
   const localStorageKey = 'walletAdapter';
   const network = clusterApiUrl('devnet');
@@ -16,6 +18,7 @@
       SlopeWalletAdapter,
       SolflareWalletAdapter,
       SolletExtensionWalletAdapter,
+      TorusWalletAdapter,
     } = await import('@solana/wallet-adapter-wallets');
 
     const walletsMap = [
@@ -23,14 +26,23 @@
       new SlopeWalletAdapter(),
       new SolflareWalletAdapter(),
       new SolletExtensionWalletAdapter(),
+      new TorusWalletAdapter(),
     ];
 
     wallets = walletsMap;
   });
+
+  $: ({ connect, select } = $walletStore);
+
+  async function connectWallet() {
+    await select(wallets[4].name);
+    await connect();
+  }
 </script>
 
 <WalletProvider {localStorageKey} {wallets} autoConnect />
 <AnchorConnectionProvider {network} {idl} />
+<button on:click={connectWallet}>connect wallet</button>
 <div>
   <slot />
 </div>
