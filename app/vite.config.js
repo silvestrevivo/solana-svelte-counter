bcab511423783968105041ac718c2f196f5b6a37
silvestrevivo <silvestrevivomillan@gmail.com>
2022-08-24 15:53:07 +0200

    feat(sveltekit): updated static adapter and vite config

--- app/vite.config.js
+++ app/vite.config.js
@@ -6,6 +6,9 @@ const config = {
 	plugins: [sveltekit()],
   optimizeDeps: {
     include: ['@project-serum/anchor', '@solana/web3.js'],
+    esbuildOptions: {
+			target: 'esnext'
+		},
   },
   resolve: {
     alias: {
@@ -16,6 +19,9 @@ const config = {
     // This makes @project-serum/anchor 's process error not happen since it replaces all instances of process.env.BROWSER with true
     'process.env.BROWSER': true
   },
+  build: {
+		target: 'esnext'
+	}
 };
 
 export default config;
